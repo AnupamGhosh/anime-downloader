f139efbc79502761f2d71895623419b08625d88e
Anupam Ghosh <i_anupam_ghosh@yahoo.co.in>
2020-05-16 09:30:09 +0530

    project init

---
+++ downloader.py
@@ -0,0 +1,291 @@
+# https://9anime.to/ajax/film/servers/yzn0 last part yzn0 can be found in the anime page url, eg. for fairy-tail nqwm
+# class server and data id = 35
+# li > a.active store data-id
+# @TODO
+# 1. have _ & ts in all ajax request
+# 2. save cookie from BASE_URL and menu-bar 
+
+from HTMLParser import HTMLParser
+import logging
+import urllib
+import httplib
+import time
+import random
+import json
+import os
+import re
+
+SERVER = 35 # mp4upload
+
+class HTMLParser9anime(HTMLParser):
+  def __init__(self):
+    HTMLParser.__init__(self)
+    self.state_mc = EpisodeInfoMachine()
+
+  def handle_starttag(self, tag, attrs):
+    self.state_mc.start_tag(tag, attrs)
+
+  def handle_endtag(self, tag):
+    self.state_mc.end_tag(tag)
+
+  def get_ep_ids(self):
+    return self.state_mc.ep_infos
+
+  @staticmethod
+  def attrs2dict(attrs):
+    return {name: value for name, value in attrs}
+
+
+class EpisodeInfoMachine:
+  def __init__(self):
+    self.ep_infos = []
+    self.SERVER = str(SERVER)
+    self.state_server = StateServer(self)
+    self.state_li = StateLi(self)
+    self.state_start = StateStart(self)
+    self.state_ul = StateUl(self)
+
+    self.state = self.state_start
+
+  def start_tag(self, tag, attrs):
+    self.state = self.state.next_state(tag, attrs)
+
+  def end_tag(self, tag):
+    self.state = self.state.next_state(tag, [], close=True)
+
+class StateStart(object):
+  def __init__(self, state_mc):
+    self.mc = state_mc
+
+  def next_state(self, tag, attrs, close=False):
+    attr = HTMLParser9anime.attrs2dict(attrs)
+    div_classes = attr.get('class', '').split(' ')
+    data_id = attr.get('data-id')
+
+    if 'server' in div_classes and data_id == self.mc.SERVER:
+      return self.mc.state_server
+    return self
+
+class StateServer(object):
+  def __init__(self, state_mc):
+    self.mc = state_mc
+
+  def next_state(self, tag, attrs, close=False):
+    attr = HTMLParser9anime.attrs2dict(attrs)
+    ul_classes = attr.get('class', '').split(' ')
+
+    if tag == 'ul' and 'episodes' in ul_classes and 'range' in ul_classes:
+      return self.mc.state_ul
+    return self
+
+class StateUl(object):
+  def __init__(self, state_mc):
+    self.mc = state_mc
+
+  def next_state(self, tag, attrs, close=False):
+    if tag == 'li' and not close:
+      return self.mc.state_li
+    if tag == 'ul' and close:
+      return self.mc.state_server
+    return self
+
+class StateLi(object):
+  def __init__(self, state_mc):
+    self.mc = state_mc
+
+  def next_state(self, tag, attrs, close=False):
+    attr = HTMLParser9anime.attrs2dict(attrs)
+
+    if tag == 'li' and close:
+      return self.mc.state_ul
+        
+    if tag == 'a' and not close:
+      self.mc.ep_infos.append(attr['data-id'])
+      assert int(attr['data-base']) == len(self.mc.ep_infos)
+    return self
+
+class Request(object):
+  def __init__(self, headers=None):
+    headers = headers or {}
+    self.headers = {
+      'accept': 'application/json, text/javascript, */*; q=0.01',
+      'x-requested-with': 'XMLHttpRequest',
+      'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36',
+      'sec-fetch-site': 'same-origin',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-dest': 'empty',
+      'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    }
+    self.headers.update(headers)
+    self.cookies = []
+    self._res_text = ''
+    
+  def save_cookie(self, key, value):
+    self.cookies.append('%s=%s' % (key, value))
+
+  def make_request(self, url, params):
+    params = params or {}
+    reg_match = re.match(r"https:\/\/([^\/]*)([^?]*)(.*)", url)
+    domain = reg_match.group(1)
+    path = reg_match.group(2)
+    temp_params = reg_match.group(3)
+    if len(temp_params) > 1:
+      temp_params = re.findall(r"([^=]+)=([^&]+)&", temp_params + '&')
+      params.update({key: val for key, val in temp_params})
+    if len(params):
+      path += '?' + urllib.urlencode(params)
+
+    con = httplib.HTTPSConnection(domain)
+    logging.debug("Requesting url: https://%s%s", domain, path)
+    headers = self.headers.copy()
+    headers['cookie'] = '; '.join(self.cookies)
+    con.request('GET', path, None, headers)
+    res = con.getresponse()
+    self._res_text = res.read()
+    con.close()
+    return res
+
+  def get(self, url, params=None):
+    self.make_request(url, params)
+    return self._res_text
+
+  def res_headers(self, url, params=None):
+    return self.make_request(url, params).getheaders()
+
+class Request9anime(Request):
+  DOMAIN = 'https://9anime.to'
+
+  def __init__(self, base_path):
+    super(Request9anime, self).__init__({
+      # 'authority': '9anime.to',
+      'referer': '%s%s' % (Request9anime.DOMAIN, base_path),
+    })
+    self.cookies = [
+      '_ga=GA1.2.1603338442.1589383036', '_gid=GA1.2.1309926967.1589383036', '_gat=1',
+      '__atuvc=1%7C20', '__atuvs=5ebc0f7bf4605059000'
+    ]
+
+  def make_request(self, path, params):
+    params = params or {}
+    params['ts'] = int(time.time())
+    url = '%s%s' % (Request9anime.DOMAIN, path)
+    return super(Request9anime, self).make_request(url, params)
+
+class Downloader(object):
+
+  def __init__(self, base_path, name_prefix, start, episode_count, save_dir):
+    self.base_path = base_path
+    self.filename_prefix = name_prefix
+    self.start_episode = start - 1
+    self.get_episodes = episode_count
+    self.save_dir = save_dir
+    self.request = Request9anime(base_path)
+    self.anime_html_filepath = os.path.join(CUR_DIR, '%s-python.html' % self.filename_prefix)
+
+  def store_cookies(self):
+    paths = [self.base_path, '/user/ajax/menu-bar']
+    for path in paths:
+      res_meta_data = self.request.res_headers(path)
+      cookies = Downloader.get_cookies(res_meta_data)
+      for key in cookies:
+        self.request.save_cookie(key, cookies[key])
+
+  @staticmethod
+  def get_cookies(res_headers):
+    cookie_headers = [val for header, val in res_headers if header == 'set-cookie']
+    cookies = {}
+    for cookie_str in cookie_headers:
+      key, val = cookie_str.split(';', 1)[0].strip().split('=', 1)
+      cookies[key] = val
+    return cookies
+
+
+  def get_episodes_html(self):
+    path_matched = re.search(r".*\.(\w+)\/(\w+)", self.base_path)
+    servers_id = path_matched.group(1)
+    episode_id = path_matched.group(2)
+    episode_url = '/ajax/film/servers'
+    content = self.request.get(episode_url, {'id': servers_id, 'episode': episode_id, '_': 887})
+
+    try:
+      html_episodes = json.loads(content)['html']
+      path = self.anime_html_filepath
+      with open(path, 'w') as html_text:
+        html_text.write(html_episodes)
+    except Exception as e:
+      logging.debug("content:\n%s", content)
+      raise e
+
+  def parse_episode_html(self):
+    parser = HTMLParser9anime()
+    path = self.anime_html_filepath
+    with open(path, 'r') as html_text:
+      html_text = open(path, 'r')
+      parser.feed(html_text.read())
+    return parser
+
+  def get_mcloudKey(self):
+    mcloud_headers = {
+      'referer': '%s%s' % (Request9anime.DOMAIN, self.base_path)
+    }
+    logging.debug('mcloud req headers=%s', Request(mcloud_headers).headers)
+    mcloud_js_val = Request(mcloud_headers).get('https://mcloud2.to/key')
+    mcloud_regex = re.search(r"mcloudKey=['\"](\w+)['\"]", mcloud_js_val)
+    mcloudKey = mcloud_regex.group(1)
+    logging.debug('mcloud=%s', mcloudKey)
+    return mcloudKey
+
+  def download_videos(self, parser):
+    # get video source html page
+    start = self.start_episode
+    get_episodes = self.get_episodes
+    anime_ep_ids = parser.get_ep_ids()[start: start + get_episodes]
+    source_info_path = '/ajax/episode/info'
+    mcloud = self.get_mcloudKey()
+    logging.debug('header:\n%s', self.request.headers)
+    for i in xrange(get_episodes):
+      logging.debug("Episode %s data-id=%s", start + i + 1, anime_ep_ids[i])
+      current_ep = start + i + 1
+      # most sensitive code
+      content = self.request.get(source_info_path, {'id': anime_ep_ids[i], 'server': SERVER, 'mcloud': mcloud, '_': 935})
+      logging.debug("source_info_url response:\n%s", content)
+      source_html_url = json.loads(content)['target']
+
+      logging.debug("Source html url: %s", source_html_url)
+      source_html_path = os.path.join(CUR_DIR, '%s-source-ep%s.html' % (self.filename_prefix, current_ep))
+      source_html = Request().get(source_html_url)
+      with open(source_html_path, 'w') as source_html_file:
+        source_html_file.write('<!-- %s -->\n' % source_html_url)
+        source_html_file.write(source_html)
+
+      html = source_html
+      video_info_match = re.search(r'.*eval.*\'(.*)\'\.split.*', html, re.MULTILINE)
+      url_info = video_info_match.group(1).split('|')
+      download_link = 'https://{subdomain}.mp4upload.com:{port}/d/{video_dir}/video.{ext}'.format(
+        subdomain=url_info[27], port=url_info[56], video_dir=url_info[55], ext=url_info[25]
+      )
+      logging.debug("Download link: %s", download_link)
+      save_as = os.path.join(self.save_dir, '%s%s.mp4' % (self.filename_prefix, current_ep))
+      returncode = os.system('wget --no-check-certificate %s -O %s' % (download_link, save_as)) and os.system(
+          'curl -k %s -o %s' % (download_link, save_as))
+      if not returncode:
+        os.remove(source_html_path)
+
+  def download(self):
+    self.store_cookies()
+    # logging.debug('9anime cookies:\n%s\n', '\n'.join(self.request.cookies))
+    self.get_episodes_html()
+    parser = self.parse_episode_html()
+    self.download_videos(parser)
+    os.remove(self.anime_html_filepath)
+
+
+logging.basicConfig(format='%(funcName)s:%(lineno)d %(levelname)s %(message)s', level=logging.DEBUG)
+CUR_DIR = os.path.dirname(__file__)
+with open(os.path.join(CUR_DIR, 'config.json'), 'r') as config_fp:
+  config = json.load(config_fp)
+BASE_PATH = config['base_path']
+Downloader(
+    BASE_PATH, config['filename_prefix'], config['start_episode'], config['get_episodes'],
+    str(config['save_in']).replace(' ', '\\ ')
+).download()
