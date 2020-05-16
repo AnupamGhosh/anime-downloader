41ed76f66f1842c971f4254673da5fccfabc3917
Anupam Ghosh <i_anupam_ghosh@yahoo.co.in>
2020-05-16 12:49:19 +0530

    move http fetch logic to make_request.py

--- downloader.py
+++ downloader.py
@@ -3,16 +3,18 @@
 # li > a.active store data-id
 # analyze every params carefully when editing
 # _ param is specific to urls and remains same for an anime page
+# Currently the _ params are pinned to fairy tail dub
+# @TODO need to find a way to automatically fetch _ from all.js
 
 from HTMLParser import HTMLParser
 import logging
-import urllib
-import httplib
 import time
 import random
 import json
 import os
 import re
+
+from make_request import Request, Request9anime
 
 SERVER = 35 # mp4upload
 
@@ -102,72 +104,6 @@ class StateLi(object):
       self.mc.ep_infos.append(attr['data-id'])
       assert int(attr['data-base']) == len(self.mc.ep_infos)
     return self
-
-class Request(object):
-  def __init__(self, headers=None):
-    headers = headers or {}
-    self.headers = {
-      'accept': 'application/json, text/javascript, */*; q=0.01',
-      'x-requested-with': 'XMLHttpRequest',
-      'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36',
-      'sec-fetch-site': 'same-origin',
-      'sec-fetch-mode': 'cors',
-      'sec-fetch-dest': 'empty',
-      'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    }
-    self.headers.update(headers)
-    self.cookies = []
-    self._res_text = ''
-    
-  def save_cookie(self, key, value):
-    self.cookies.append('%s=%s' % (key, value))
-
-  def make_request(self, url, params):
-    params = params or {}
-    reg_match = re.match(r"https:\/\/([^\/]*)([^?]*)(.*)", url)
-    domain = reg_match.group(1)
-    path = reg_match.group(2)
-    temp_params = reg_match.group(3)
-    if len(temp_params) > 1:
-      temp_params = re.findall(r"([^=]+)=([^&]+)&", temp_params + '&')
-      params.update({key: val for key, val in temp_params})
-    if len(params):
-      path += '?' + urllib.urlencode(params)
-
-    con = httplib.HTTPSConnection(domain)
-    logging.debug("Requesting url: https://%s%s", domain, path)
-    headers = self.headers.copy()
-    headers['cookie'] = '; '.join(self.cookies)
-    con.request('GET', path, None, headers)
-    res = con.getresponse()
-    self._res_text = res.read()
-    con.close()
-    return res
-
-  def get(self, url, params=None):
-    self.make_request(url, params)
-    return self._res_text
-
-  def res_headers(self, url, params=None):
-    return self.make_request(url, params).getheaders()
-
-class Request9anime(Request):
-  DOMAIN = 'https://9anime.to'
-
-  def __init__(self, base_path):
-    super(Request9anime, self).__init__({
-      'referer': '%s%s' % (Request9anime.DOMAIN, base_path),
-    })
-    self.cookies = [
-      '_ga=GA1.2.1603338442.1589383036', '_gid=GA1.2.1309926967.1589383036', '_gat=1',
-      '__atuvc=1%7C20', '__atuvs=5ebc0f7bf4605059000'
-    ]
-
-  def make_request(self, path, params):
-    params = params or {}
-    params['ts'] = int(time.time())
-    url = '%s%s' % (Request9anime.DOMAIN, path)
-    return super(Request9anime, self).make_request(url, params)
 
 class Downloader(object):
 
